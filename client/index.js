b5c2cffccf99df3b721fac097754935d397995e8
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2022-11-25 11:41:55 +0200

    Move styles into SCSS file

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import Root from '/root';
 
+import './style.scss';
+
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 
 root.render( <Root /> );
